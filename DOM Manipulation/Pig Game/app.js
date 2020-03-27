d826678ebd29eda79785a57eae1a06451327e03b
Sahil Saini <devosahil@gmail.com>
2020-03-27 22:48:43 +0530

    Pig Game Finished Code

--- DOM Manipulation/Pig Game/app.js
+++ DOM Manipulation/Pig Game/app.js
@@ -1,18 +1,101 @@
 /*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
+    Pig Game
 */
 
-var score, currentPlayer, dice, roundScore, globalScore;
+// Declare the required variables
+var score, currentPlayer, roundScore, gamePlaying;
 
-score = [0, 0];
-currentPlayer = 0;
-roundScore = 0;
+initializeGame();
 
-dice = Math.floor(Math.random() * 6) + 1;
+// Add click eventListener to the "Roll Dice" button
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    if( gamePlaying ) {
+        // Generate a random number and store it in dice
+        var dice = Math.floor(Math.random() * 6) + 1;
+
+        // Display the result
+        diceObj = document.querySelector('.dice');
+        diceObj.style.display = 'block';
+        diceObj.src = 'dice-' + dice + '.png';
+
+        // Update the result
+        if(dice !== 1) {
+            roundScore += dice;
+            document.getElementById('current-' + currentPlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    if( gamePlaying ) {
+        // add currentScore to the globalScore
+        score[currentPlayer] += roundScore;
+
+        document.querySelector('#score-' + currentPlayer).textContent = score[currentPlayer];
+
+        // If user won the game
+        if( score[currentPlayer] >= 100 ) {
+            document.getElementById('name-' + currentPlayer).textContent = 'Winner';
+            document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.player-' + currentPlayer + '-panel').classList.add('winner');
+            document.querySelector('.player-' + currentPlayer + '-panel').classList.remove('active');
+
+            // Set gamePlaying state variable to false
+            gamePlaying = false;
+        } else {
+            // Next player's turn
+            nextPlayer();
+        }
+    }  
+});
+
+
+// "New Game" button
+document.querySelector('.btn-new').addEventListener('click', initializeGame);
+
+// Function for next player's turn
+function nextPlayer() {
+    // Next player's turn
+    currentPlayer === 0 ? currentPlayer = 1 : currentPlayer = 0;
+    roundScore = 0;
+
+    // Set current score for both players to zero
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // Toggle active class on players
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+// Game Initialization Function
+function initializeGame() {
+    // Assign values to the variables
+    score = [0, 0];
+    currentPlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+
+    // Set all scores to 0
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    // hide the dice image, only display it when the 'Roll Dice' button is clicked
+    document.querySelector('.dice').style.display = 'none';
+
+    // Change player text back from winner
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+
+    // Update classes and set player-0 as active
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+
+}
